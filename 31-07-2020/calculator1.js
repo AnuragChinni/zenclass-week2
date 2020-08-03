944f779269989fc71f6b9c5137fd382584e02b95
AnuragChinni <67939056+AnuragChinni@users.noreply.github.com>
2020-08-03 21:11:49 +0530

    Add files via upload

---
+++ 31-07-2020/calculator1.js
@@ -0,0 +1,35 @@
+class calculator{
+	
+constructor(num1,num2){
+				
+	this.num1=num1;
+			
+	this.num2= num2;
+			
+	
+	}
+
+add(){
+console.log(this.num1+this.num2);
+}
+mul(){
+console.log(this.num1*this.num2);
+}
+sub(){
+console.log(this.num1-this.num2);
+}
+quot(){
+console.log(this.num1/this.num2);
+}
+remainder(){
+console.log(this.num1%this.num2);
+}
+}
+let calc = new calculator(2,3);
+calc.add();
+calc.sub();
+calc.mul();
+calc.quot();
+calc.remainder();
+
+	
---
+++ 31-07-2020/movie1.js
@@ -0,0 +1,66 @@
+class movie{
+	
+constructor(title, Studio, rating='PG'){
+				
+	this.title=title;
+			
+	this.Studio= Studio;
+			
+	this.rating=rating;
+				
+	
+			}
+
+	
+static arr1() {
+			 	
+let arr=[];
+			 
+	for(let i=0; i<arguments.length; i++){
+			 		
+arr.push(arguments[i]);
+  }
+
+			 	
+return arr;
+
+  }
+  
+static arr3(){
+  	
+let arr=[];
+ 
+ 	for(let i=0; i<arguments.length; i++){
+  		          
+  if(arguments[i].rating==='PG'){
+  		            	
+arr.push(arguments[i]);
+  		      
+      }
+			
+}
+	
+ return arr;
+ 
+ }
+
+ 
+  
+}
+
+
+let  slnk = new movie('slnk', 'abc',8);
+
+let cde= new movie ('cde', 'def');
+
+let fghi = new movie ('fghi', 'jkl');
+
+let casino_Royale = new movie ('casino_Royale','Eon productions','PG13');
+let moviearr= movie.arr1(slnk,cde,fghi,casino_Royale);
+
+let ratingarr= movie.arr3(slnk,cde,fghi,casino_Royale);
+
+
+console.log(moviearr);
+
+console.log(ratingarr);
